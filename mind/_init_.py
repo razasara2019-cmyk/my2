10c9646b3538efe17f0db2cebbf8477f9f3c4f81
razasara2019-cmyk <razasara2019@gmail.com>
2026-05-24 13:00:52 +0100

    Create _init_.py

---
+++ mind/_init_.py
@@ -0,0 +1,14 @@
+"""
+mind – حزمة الوعي والذاكرة للكيان الدستوري الحي
+الإصدار: 5.0a النهائي
+"""
+
+from mind.graph_mind import GraphMind
+from mind.alignment_instinct import AlignmentInstinct
+from mind.internal_state import InternalState
+
+__all__ = [
+    "GraphMind",
+    "AlignmentInstinct",
+    "InternalState"
+]
